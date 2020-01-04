50a037cf2db103bf2901525676c59bddd375124d
Ashok Kumar <ashok.volktek@gmail.com>
2020-01-04 12:49:58 +0530

    first commit for hellow world java/py project

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("ENTER VALUE:"))
+
+## print('Factoraial of', a , 'is', math.factorial(a))
+## print('ceil of x', math.ceil(a))
+## print('comb value', math.fabs(a))
+print('floor value:', math.floor(a))
